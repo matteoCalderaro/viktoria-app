391ea1e8171141569dce91d494ea5b463199f630
Matteo Calderaro <matteocalderaro@gmail.com>
2022-05-20 18:02:19 +0200

    last commit3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,37 +4,39 @@ import { useState } from "react";
 
 export default function About(props) {
   const [todos] = useState([
+    {
+        id: 1,
+        name: "Cazzeggiare e gironzolare per il Garda trentino",
+        isComplete: true,
+        isEditing: false
+      },
+      {
+        id: 2,
+        name: "Parlare in italiano il più possile (ma non sempre)",
+        isComplete: true,
+        isEditing: false
+      },
 
     {
-      id: 2,
+      id: 5,
       name: "Mai bere il capuccino dopo lo 11:00 (ordinare Aperol Spritz)",
       isComplete: false,
       isEditing: false
     },
     {
-      id: 3,
+      id: 4,
       name: "Non cenare mai prima delle 19:30 (vietatissimo!)",
       isComplete: true,
       isEditing: false
     },
     {
-      id: 4,
+      id: 3,
       name: "Non farsi rubare la bicicletta",
       isComplete: true,
       isEditing: false
     },
-    {
-      id: 5,
-      name: "Parlare in italiano il più possile (ma non sempre)",
-      isComplete: true,
-      isEditing: false
-    },
-    {
-      id: 6,
-      name: "Cazzeggiare e gironzolare per il Garda trentino",
-      isComplete: true,
-      isEditing: false
-    },
+
+
 
   ]);
   return (
@@ -57,7 +59,7 @@ export default function About(props) {
                 <ul className="listaTodos">
                   <li>{e.name}</li>
                 </ul>
-              )).sort((a,b)=>b-a)}
+              ))}
             </div>
           </CSSTransition>
         <CSSTransition
